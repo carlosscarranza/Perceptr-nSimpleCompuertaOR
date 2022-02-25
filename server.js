3ff472acf1f7d7b1b50d3592bd1a391ec47c8024
carlosscarranza <carlosscarranza@yahoo.com>
2022-02-24 20:11:08 -0500

    add

--- server.js
+++ server.js
@@ -8,39 +8,55 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var counter = 0;
+
 var gweight1 = 0;
 var gweight2 = 0;
 var gerror = 0;
 
 app.get('/', async function (req, res) {
 
+    gweight1 = req.body.weight1;
+    gweight2 = req.body.weight2;
+    gerror = req.body.error;
+
+    calculate(gerror, req.body.learning_factor, gweight1, gweight2);
+
+    res.send({
+        w1: gweight1,
+        w2: gweight2,
+        error: gerror,
+        counterAdjust: counter
+    })
 })
 
-app.get('/backup', function (req, res) {
+app.get('/validate', function (req, res) {
 
+    let number =  Math.tanh( ((req.body.x1 * req.body.weight1) + (req.body.x2 * req.body.weight2))   - (req.body.error))
+    res.send({number});
 })
 
 const calculate = (error, learning_factor, weight1, weight2) => {
 
-    let y1 = validate(1, weight1, 1, weight2, 1);
+    let y1 = validate(1, weight1, 1, weight2, error, 1);
     if(y1 === false){ 
         adjust(weight1, weight2, error, learning_factor, 1, 1, 1)
         calculate(gerror, learning_factor, gweight1, gweight2);
     }
-
-    let y2 = validate(1, weight1, -1, weight2, 1);
+    
+    let y2 = validate(1, weight1, -1, weight2, error, 1);
     if(y2 === false){ 
         adjust(weight1, weight2, error, learning_factor, 1, 1, -1)
         calculate(gerror, learning_factor, gweight1, gweight2);
     }
-
-    let y3 = validate(-1, weight1, 1, weight2, 1);
+    
+    let y3 = validate(-1, weight1, 1, weight2, error, 1);
     if(y3 === false){ 
         adjust(weight1, weight2, error, learning_factor, 1, -1, 1)
         calculate(gerror, learning_factor, gweight1, gweight2);
     }
-
-    let y4 = validate(-1, weight1, -1, weight2, -1);
+    
+    let y4 = validate(-1, weight1, -1, weight2, error, -1);
     if(y4 === false){ 
         adjust(weight1, weight2, error, learning_factor, -1, -1, -1)
         calculate(gerror, learning_factor, gweight1, gweight2);
@@ -49,15 +65,16 @@ const calculate = (error, learning_factor, weight1, weight2) => {
 
 const validate = (x1, weight1, x2, weight2, error, expected) => {
 
-    let y = Math.tanh( ((x1 * weight1) + (x2 * weight2)) - error) 
-    return expected === 1 ? y >= 0 : y <= 0;
+    let y = Math.tanh( ((x1 * weight1) + (x2 * weight2))  - (error))
+    return expected == 1 ? y >= 0 : y <= 0
 }
 
 const adjust = (weight1, weight2, error, learning_factor, y, x1, x2) => {
 
     gweight1 = weight1 + (2 * learning_factor * y  * x1);
     gweight2 = weight2 + (2 * learning_factor * y  * x2);
-    gerror = error + (2 * learning_factor * y  * (-1));
+    gerror   = error   + (2 * learning_factor * y  * (-1));
+    counter++;
 }
 
 let port = process.env.PORT || 5000;
